3ed8a4b0049cfec6aa977af76617dc2818993646
Emmnux <emna.walha@insat.ucar.tn>
2024-04-02 15:38:20 +0100

    related products in single_product.php is done

--- server/place_order.php
+++ server/place_order.php
@@ -1,26 +1,15 @@
 <?php
 
 include 'connection.php';
-
 session_start();
 
-
-//if user is not logged in
+// If user is not logged in
 if(!isset($_SESSION['logged_in'])){
-
     header('location: ../checkout.php?message=fail');
     exit();
-
-
-    //if user is logged in
-}else{
-
-
+} else {
     if(isset($_POST['place_order'])){
-
-
         // Step 1: Get user info and store it in the database
-    
         $name = $_POST['name']; 
         $email = $_POST['email'];
         $phone = $_POST['phone'];
@@ -28,59 +17,51 @@ if(!isset($_SESSION['logged_in'])){
         $address = $_POST['address']; 
         $order_cost = $_SESSION['total']; 
         $order_status = 'on-hold'; 
-        $user_id = $_SESSION['user_id']; 
         $order_date = date('Y-m-d H:i:s'); 
-    
-        //insert user info into the orders table
-    
-        $stmt = $conn->prepare("INSERT INTO orders (order_cost, order_status, user_id, user_phone, user_city, user_address, order_date) VALUES (?,?,?,?,?,?,?)");
-        $stmt->bind_param('isiisss', $order_cost, $order_status, $user_id, $phone, $city, $address, $order_date);
-        
-        $stmt_status = $stmt->execute();
-    
-        if(!$stmt_status){
-            header('location: ../index.php');
+
+        // Check if user_id is set and not null
+        if(isset($_SESSION['user_id']) && $_SESSION['user_id'] !== null) {
+            $user_id = $_SESSION['user_id']; 
+
+            // Insert user info into the orders table
+            $stmt = $conn->prepare("INSERT INTO orders (order_cost, order_status, user_id, user_phone, user_city, user_address, order_date) VALUES (?,?,?,?,?,?,?)");
+            $stmt->bind_param('issssss', $order_cost, $order_status, $user_id, $phone, $city, $address, $order_date);
+
+            $stmt_status = $stmt->execute();
+
+            if(!$stmt_status){
+                header('location: ../index.php');
+                exit();
+            }	
+
+            $order_id = $stmt->insert_id; // Get the auto-generated order ID
+
+            // Step 2: Get cart items and store them in the database
+            $cart = $_SESSION['cart']; 
+            foreach($cart as $key => $value){
+                $product = $_SESSION['cart'][$key];
+                $product_id = $product['product_id'];
+                $product_name = $product['product_name']; 
+                $product_image = $product['product_image']; 
+                $product_price = $product['product_price'];
+                $product_quantity = $product['product_quantity']; 
+
+                // Insert cart items into the order_items table
+                $stmt1 = $conn->prepare("INSERT INTO order_items (order_id, product_id, product_name, product_image,product_price, product_quantity, user_id, order_date) VALUES (?,?,?,?,?,?,?,?)");
+                $stmt1->bind_param('iissiiis', $order_id, $product_id, $product_name, $product_image, $product_price, $product_quantity, $user_id, $order_date);
+                $stmt1->execute();
+            }
+
+            // Step 3: Empty the cart --> delay until payment is done
+
+            // Step 4: Inform the user whether the order is placed or not 
+            header('location: ../payment.php?order_status=order placed successfully');
+        } else {
+            // Redirect user to login page if user_id is not set
+            header('location: ../login.php');
             exit();
-        }	
-    
-        $order_id = $stmt->insert_id; // Get the auto-generated order ID
-    
-    
-    
-        // Step 2: Get cart items and store them in the database
-    
-        $cart = $_SESSION['cart']; 
-        foreach($cart as $key => $value){
-            $product = $_SESSION['cart'][$key];
-            $product_id = $product['product_id'];
-            $product_name = $product['product_name']; 
-            $product_image = $product['product_image']; 
-            $product_price = $product['product_price'];
-            $product_quantity = $product['product_quantity']; 
-    
-            //insert cart items into the order_items table
-    
-            $stmt1 = $conn->prepare("INSERT INTO order_items (order_id, product_id, product_name, product_image,product_price, product_quantity, user_id, order_date) VALUES (?,?,?,?,?,?,?,?)");
-            $stmt1->bind_param('iissiiis', $order_id, $product_id, $product_name, $product_image, $product_price, $product_quantity, $user_id, $order_date);
-            $stmt1->execute();
         }
-    
-    
-    
-        // Step 3: Empty the cart --> delay until payement is done
-    
-    
-        // step 4 : inform user whether the order is placed or not 
-        header('location: ../payement.php?order_status=order placed successfully');
-    
-    
     }
-
-
-
 }
-
-
-
 
 ?>
--- single_product.php
+++ single_product.php
@@ -1,165 +1,116 @@
 <?php 
-
-
 session_start(); 
-
-
 include('server/connection.php');
 
-
 if(isset($_GET['product_id'])){
-
     $product_id = $_GET['product_id'];
 
     $stmt = $conn->prepare("SELECT * FROM products WHERE product_id = ?");
-    $stmt->bind_param("i",$product_id);
+    $stmt->bind_param("i", $product_id);
     
     $stmt->execute();
 
-    $product = $stmt->get_result();
+    $result = $stmt->get_result();
 
+    // Fetch the product details as an associative array
+    $product = $result->fetch_assoc();
 
+    // Check if product exists
+    if(!$product) {
+        // Redirect if product not found
+        header('location: index.php');
+        exit(); // Terminate script execution
+    }
 }else{
+    // Redirect if product_id is not set
     header('location: index.php');
+    exit(); // Terminate script execution
 }
-
-
-
-
 ?>
 
-
-
-
 <?php include('layouts/header.php'); ?>
 
-
-    <!--Single Product-->
-    <section class="container single-product my-5 pt-5">
-        <div class="row mt-5">
-        
-        <?php while($row = $product->fetch_assoc()){ ?>
-           
-            <div class="col-lg-5 col-md-6 col-sm-12">
-                <img class="img-fluid w-100 pb-1" src="/assets/imgs/<?php echo $row['product_image']; ?>" alt="" id="mainImg">
-                <div class="small-img-group">
-                    <div class="small-img-col">
-                        <img src="/assets/imgs/<?php echo $row['product_image']; ?>" width="100%" class="small-img">
-                    </div>
-                    <div class="small-img-col">
-                        <img src="/assets/imgs/<?php echo $row['product_image2']; ?>" width="100%" class="small-img">
-                    </div>
-                    <div class="small-img-col">
-                        <img src="/assets/imgs/<?php echo $row['product_image3']; ?>" width="100%" class="small-img">
-                    </div>
-                    <div class="small-img-col">
-                        <img src="/assets/imgs/<?php echo $row['product_image4']; ?>" width="100%" class="small-img">
-                    </div>
+<!-- Single Product -->
+<section class="container single-product my-5 pt-5">
+    <div class="row mt-5">
+        <div class="col-lg-5 col-md-6 col-sm-12">
+            <img class="img-fluid w-100 pb-1" src="/assets/imgs/<?php echo $product['product_image']; ?>" alt="" id="mainImg">
+            <div class="small-img-group">
+                <div class="small-img-col">
+                    <img src="/assets/imgs/<?php echo $product['product_image']; ?>" width="100%" class="small-img">
+                </div>
+                <div class="small-img-col">
+                    <img src="/assets/imgs/<?php echo $product['product_image2']; ?>" width="100%" class="small-img">
+                </div>
+                <div class="small-img-col">
+                    <img src="/assets/imgs/<?php echo $product['product_image3']; ?>" width="100%" class="small-img">
+                </div>
+                <div class="small-img-col">
+                    <img src="/assets/imgs/<?php echo $product['product_image4']; ?>" width="100%" class="small-img">
                 </div>
             </div>
+        </div>
 
-            <div class="col-lg-6 col-12">
-                <h6>Men/Shoes</h6>
-                <h3 class="py-4"><?php echo $row['product_name']; ?></h3>
-                <h2>$<?php echo $row['product_price']; ?></h2>
-                <form method="POST" action="cart.php">
-                <input type="hidden" name="product_id" value="<?php echo $row['product_id']; ?>"/>
-                <input type="hidden" name="product_image" value="<?php echo $row['product_image']; ?>"/>
-                <input type="hidden" name="product_name" value="<?php echo $row['product_name']; ?>"/>
-                <input type="hidden" name="product_price" value="<?php echo $row['product_price']; ?>"/>
+        <div class="col-lg-6 col-12">
+            <h6>Men/Shoes</h6>
+            <h3 class="py-4"><?php echo $product['product_name']; ?></h3>
+            <h2>$<?php echo $product['product_price']; ?></h2>
+            <form method="POST" action="cart.php">
+                <input type="hidden" name="product_id" value="<?php echo $product['product_id']; ?>"/>
+                <input type="hidden" name="product_image" value="<?php echo $product['product_image']; ?>"/>
+                <input type="hidden" name="product_name" value="<?php echo $product['product_name']; ?>"/>
+                <input type="hidden" name="product_price" value="<?php echo $product['product_price']; ?>"/>
                 <input type="number" name="product_quantity" value="1">
-                
-                <button class="buy-btn" name="add_to_cart" type="submit">ADD TO CART</button></form> 
-                <h4 class="my-5 ">Product details</h4>
-                <span><?php echo $row['product_description']; ?>
-                </span>
+                <button class="buy-btn" name="add_to_cart" type="submit">ADD TO CART</button>
+            </form> 
+            <h4 class="my-5">Product details</h4>
+            <span><?php echo $product['product_description']; ?></span>
+        </div>
+    </div>
+</section>
+
+<!-- Related Products -->
+<section id="related-products" class="my-5 pb-5">
+    <div class="container text-center mt-5 py-5">
+        <h3>Related Products</h3>
+        <hr class="mx-auto">
+    </div>
+    <div class="row mx-auto container-fluid">
+        <?php
+        // Retrieve related products from the same category
+        $stmt_related = $conn->prepare("SELECT * FROM products WHERE product_category = ? AND product_id != ? LIMIT 4");
+        $stmt_related->bind_param("si", $product['product_category'], $product_id);
+        $stmt_related->execute();
+        $result_related = $stmt_related->get_result();
+
+        while ($row_related = $result_related->fetch_assoc()) {
+        ?>
+            <div class="product text-center col-lg-3 col-md-4 col-sm-12">
+                <img src="/assets/imgs/<?php echo $row_related['product_image']; ?>" class="img-fluid mb-3">
+                <div class="star">
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                </div>
+                <h5 class="p-name"><?php echo $row_related['product_name']; ?></h5>
+                <h4 class="p-price">$<?php echo $row_related['product_price']; ?></h4>
+                <button class="buy-btn">BUY NOW</button>
             </div>
-            
-
-            
-
-
         <?php } ?>
-        </div>
-    </section>
+    </div>
+</section>
 
+<script>
+    var mainImg = document.getElementById('mainImg');
+    var smallImg = document.getElementsByClassName('small-img');
 
-    <!--Related Products-->
-    <section id="related-products" class="my-5 pb-5">
-        <div class="container text-center mt-5 py-5">
-            <h3>Related Products</h3>
-            <hr class="mx-auto">
-        </div>
-        <div class="row mx-auto container-fluid">
-            <div class="product text-center col-lg-3 col-md-4 col-sm-12">
-                <img src="assets/imgs/featured1.png" class="img-fluid mb-3">
-                <div class="star">
-                    <i class="fa fa-star"></i>
-                    <i class="fa fa-star"></i>
-                    <i class="fa fa-star"></i>
-                    <i class="fa fa-star"></i>
-                    <i class="fa fa-star"></i>
-                </div>
-                <h5 class ="p-name">Sports Shoes</h5>
-                <h4 class ="p-price">$199.9</h4>
-                <button class="buy-btn">BUY NOW</button>
-            </div>
-            <div class="product text-center col-lg-3 col-md-4 col-sm-12">
-                <img src="assets/imgs/featured2.png" class="img-fluid mb-3">
-                <div class="star">
-                    <i class="fa fa-star"></i>
-                    <i class="fa fa-star"></i>
-                    <i class="fa fa-star"></i>
-                    <i class="fa fa-star"></i>
-                    <i class="fa fa-star"></i>
-                </div>
-                <h5 class ="p-name">Sports Shoes</h5>
-                <h4 class ="p-price">$199.9</h4>
-                <button class="buy-btn">BUY NOW</button>
-            </div>
-            <div class="product text-center col-lg-3 col-md-4 col-sm-12">
-                <img src="assets/imgs/featured3.png" class="img-fluid mb-3">
-                <div class="star">
-                    <i class="fa fa-star"></i>
-                    <i class="fa fa-star"></i>
-                    <i class="fa fa-star"></i>
-                    <i class="fa fa-star"></i>
-                    <i class="fa fa-star"></i>
-                </div>
-                <h5 class ="p-name">Sports Shoes</h5>
-                <h4 class ="p-price">$199.9</h4>
-                <button class="buy-btn">BUY NOW</button>
-            </div>
-            <div class="product text-center col-lg-3 col-md-4 col-sm-12">
-                <img src="assets/imgs/featured4.png" class="img-fluid mb-3">
-                <div class="star">
-                    <i class="fa fa-star"></i>
-                    <i class="fa fa-star"></i>
-                    <i class="fa fa-star"></i>
-                    <i class="fa fa-star"></i>
-                    <i class="fa fa-star"></i>
-                </div>
-                <h5 class ="p-name">Sports Shoes</h5>
-                <h4 class ="p-price">$199.9</h4>
-                <button class="buy-btn">BUY NOW</button>
-            </div>
-        </div>
-    </section>
-
-
-
-
-
-    <script>
-        var mainImg = document.getElementById('mainImg');
-        var smallImg = document.getElementsByClassName('small-img');
-
-        for(let i=0; i<smallImg.length; i++){
-            smallImg[i].onclick = function(){
-                mainImg.src = smallImg[i].src;
-            }
+    for(let i=0; i<smallImg.length; i++){
+        smallImg[i].onclick = function(){
+            mainImg.src = smallImg[i].src;
         }
+    }
+</script>
 
-    </script>
-
-<?php include('layouts/header.php'); ?>
+<?php include('layouts/footer.php'); ?>
