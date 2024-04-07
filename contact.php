c02501ec92f28879f415425a2f7499f9ce8d41f5
Mariem Abida <mariem.abida@insat.ucar.tn>
2024-04-07 03:48:57 +0100

    Contact us
    modification de style.css +contact.php+sendemail.php(en cours)

--- contact.php
+++ contact.php
@@ -1,35 +1,54 @@
 <?php
+session_start();
+
+
+
+include 'server/connection.php';
 
 
 include('layouts/header.php'); 
 
-session_start(); 
+ ?>
 
 
-?>
+
 
 
 
 
 
     <!--Contact-->
-    <section id="contact" class="container my-5 py-5">
-        <div class="container text-center mt-5">
-            <h3>Contact us</h3>
+    <section class="my-5 py-5">
+        <div class="container text-center mt-3 pt-5">
+            <h2 class="form-weight-bold">Contact Us</h2>
             <hr class="mx-auto">
-            <p class="w-50 mx-auto">
-                Phone number : <span>123 456 789</span>
-            </p>
-            <p class="w-50 mx-auto">
-                Email address : <span>info@email.com</span>
-            </p>
-            <p class="w-50 mx-auto">
-                We work 24/7 to answer your questions
-            </p>
+        </div>
+        <div class="mx-auto container">
+            <form id="contact-form" method="POST" action="sendemail.php">
+                
+                <div class="form-group">
+                    <label>Name</label>
+                    <input type="text" class="form-control" id="name" name="name" placeholder="Name" required>
+                </div>
+                <div class="form-group">
+                    <label>Email address</label>
+                    <input type="email" class="form-control" id="email" name="email" placeholder="Email address" required>
+                </div>
+                <div class="form-group">
+                    <label>Subject</label>
+                    <input type="text" class="form-control" id="subject" name="subject" placeholder="Subject" required>
+                </div>
+                <div class="form-group">
+                    <label>Message</label>
+                    <textarea class="form-control"  id="message" name="message" placeholder="Leave a message"required></textarea>
+                </div>
+                <div class="form-group">
+                    <input type="submit" name="send" class="btn" id="send-btn" value="Send">
+                </div>
+                
+            </form>
         </div>
     </section>
-
-
 
 
 
---
+++ sendemail.php
@@ -0,0 +1,5 @@
+<?php
+$name=$_post["name"];
+$email=$_post["email"];
+$subject=$_post["subject"];
+$message=$_post["message"];
