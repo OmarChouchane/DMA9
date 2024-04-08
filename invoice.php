0c881990bdba66f684565935b1eaf67c032ed31c
Khemiri-sahar <164678308+Khemiri-sahar@users.noreply.github.com>
2024-04-09 01:43:06 +0200

    Dompdf

--- invoice.php
+++ invoice.php
@@ -1,3 +1,18 @@
 <?php
-    //require_once 'dompdf/vendor/autoload.inc.php';;
+    require __DIR__. '/vendor/autoload.php';
+
+    use Dompdf\Dompdf;
+    
+    $title = '<h1 style="font-family: Poppins;">Taste The Best Food in Town .</h1>';
+    $title .='<img src="assets/imgs/logo.png">';
+
+    $dompdf = new Dompdf ([
+        "chroot" => __DIR__
+    ]);
+
+    $dompdf ->loadHtml($title);
+
+    $dompdf ->render();
+
+    $dompdf ->stream("Facture.pdf");
 ?>
