82ceaa03b934c38e96e3971032323a5f76e91b93
OmarChouchane <omar.ch52831@gmail.com>
2024-05-10 18:49:44 +0100

    contact us section
    home page section

--- index.php
+++ index.php
@@ -11,6 +11,8 @@ include('layouts/header.php');
 
     <div id="banner-bg" class="container-fluid">
 
+
+
         <!--Home--> 
         <section id="home" data-aos="fade-up"  data-aos-duration="2000">
                 <div class="container">
@@ -61,9 +63,7 @@ include('layouts/header.php');
         </section>
 
     
-    
-    
-    
+
     
         <!--Popular-->
         <section id="popular">
@@ -103,8 +103,6 @@ include('layouts/header.php');
                 </div>
             </div>
         </section>
-
-
 
 
 
@@ -186,60 +184,68 @@ include('layouts/header.php');
 
 
 
-        <!--Features-->
-        <section id="features" class="py-5">
-            <div class="container px-5">
-                <div class="row gx-5 align-items-center">
-                    <div class="col-lg-8 order-lg-1 mb-5 mb-lg-0">
-                        <div class="container-fluid px-5">
-                            <div class="row gx-5">
-                                <div class="col-md-6 mb-5" data-aos="fade-left" data-aos-duration="1000">
-                                    <!-- Feature item-->
-                                    <div class="text-center">
-                                        <i class="bi bi-stopwatch icon-feature text-gradient d-block mb-3"></i>
 
-                                        <h3 class="font-alt">Sor3a</h3>
-                                        <p class="text-muted mb-0">prioritizing speed without compromising quality.</p>
-                                    </div>
-                                </div>
-                                <div class="col-md-6 mb-5" data-aos="fade-left" data-aos-duration="1000">
-                                    <!-- Feature item-->
-                                    <div class="text-center">
-                                        <i class="bi bi-emoji-laughing icon-feature text-gradient d-block mb-3"></i>
-                                        <h3 class="font-alt">Bachecha</h3>
-                                        <p class="text-muted mb-0"> Serving smiles with every dish.</p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="col-md-6 mb-5 mb-md-0" data-aos="fade-left" data-aos-duration="1000">
-                                    <!-- Feature item-->
-                                    <div class="text-center">
-                                        <i class="bi bi-check-lg icon-feature text-gradient d-block mb-3"></i>
-                                        <h3 class="font-alt">Mawthou9</h3>
-                                        <p class="text-muted mb-0">Selecting each ingredient, aiming confidence in our dishes.</p>
-                                    </div>
-                                </div>
-                                <div class="col-md-6" data-aos="fade-left" data-aos-duration="1000">
-                                    <!-- Feature item-->
-                                    <div class="text-center">
-                                        <i class="bi bi-cash icon-feature text-gradient d-block mb-3"></i>
-                                        <h3 class="font-alt">R5is</h3>
-                                        <p class="text-muted mb-0">Affordable for all.</p>
-                                    </div>
-                                </div>
-                            </div>
+        <!--Contact Us-->
+
+  
+
+        <section class="pb-5" id="contact-us">
+            <div class="container" data-aos="zoom-in-up" data-aos-duration="1000">
+                <div class="body">
+                <div class="row justify-content-center text-center mb-lg-5">
+                    <div class="col-lg-8 col-xxl-7">
+                    <div class="container text-center mt-5"  data-aos="fade-up" data-aos-duration="1000">
+                            <h3>Contact Us</h3>
+                            <hr class="mx-auto">
+                            <!--<p>Here you can check out our featured products</p>-->
                         </div>
                     </div>
-                    <div class="col-lg-4 order-lg-0">
-                        <!-- Features section device mockup-->
+                </div>
+                <div class="row pb-5 ">
+                    
+                        <div class="col-md-6 map" data-aos="zoom-in-up" data-aos-duration="1000">
+                        
+                        
+                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d10739.607942007151!2d10.19700484579142!3d36.8447621029688!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12fd34c6d1e93bef%3A0x4153c4733f285343!2sNational%20Institute%20of%20Applied%20Science%20and%20Technology!5e0!3m2!1sen!2stn!4v1715343618339!5m2!1sen!2stn" width="100%" height="340" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>  
                         
                     </div>
+
+                    <div class="col-md-6" data-aos="zoom-in-up" data-aos-duration="1000">
+                    
+                        <form>
+                            <div class="row text-center">
+                            
+                                <div class="col-12">
+                                
+                                    <div class="mb-3">
+                                        <input class="form-control bg-light" placeholder="Full name" type="text">
+                                    </div>
+                                </div>
+                                <div class="col-12">
+                                    <div class="mb-3">
+                                        <input class="form-control bg-light" placeholder="Email address" type="email">
+                                    </div>
+                                </div>
+                                <div class="col-12">
+                                    <div class="mb-3">
+                                        <textarea class="form-control bg-light" placeholder="Your message" rows="5"></textarea>
+                                    </div>
+                                </div>
+                                <div class="col-lg-12 send">
+                                    <div class="">
+                                        <button class="btn btn-primary submit" type="submit">Send message</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
                 </div>
             </div>
         </section>
+
+
     
-        
     
     
     </div>
